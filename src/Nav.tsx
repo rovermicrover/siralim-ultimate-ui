2c41e581ee9145b355b5108bc720fa27ffd1c5fe
Alex Gurganus <gurganusalex@gmail.com>
2021-10-03 12:48:50 -0500

    refactor: merge svg and text string image sources

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Drawer from "@mui/material/Drawer";
 import Toolbar from "@mui/material/Toolbar";
-import { Tooltip } from "@mui/material";
+import { SvgIconTypeMap, Tooltip } from "@mui/material";
 import Link from "@mui/material/Link";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
@@ -28,18 +28,18 @@ import AuthorJpg from "./images/nav/author.jpg";
 import SteamJpg from "./images/nav/steam.jpg";
 
 import SourceIcon from "@mui/icons-material/Source";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
 
 class RouteData {
   constructor(
     public route: string,
     public title: string,
-    public iconSrc: string
+    public iconSrc: string | OverridableComponent<SvgIconTypeMap<{}, "svg">>
   ) {}
 }
 
-interface ISourceLinkData {
-  title: string;
-  url: string;
+function isstring(arg: any): arg is string {
+  return typeof arg === "string";
 }
 
 interface IIconLinkData {
@@ -65,32 +65,16 @@ export default function Nav({
   const itemTextStyle = { lineHeight: `${iconWidth}px`, margin: "0px" };
   const drawerWidth = isNavOpen ? 240 : iconWidth + 26;
 
-  function sourceLinksToIconLinks(links: ISourceLinkData[]): IIconLinkData[] {
-    return links.map((link) => {
-      const icon = <SourceIcon sx={{ fontSize: `${iconWidth}px` }} />;
-
-      return { title: link.title, url: link.url, icon: icon };
-    });
-  }
-
   function routeDataToIconLinks(routeDatas: RouteData[]): IIconLinkData[] {
     return routeDatas.map((data) => {
-      const icon = (
-        <img src={data.iconSrc} width={iconWidth} alt={data.title} />
-      );
-
-      return { title: data.title, url: data.route, icon: icon };
-    });
-  }
-
-  function externalLinkstoIconLinks(
-    externalLinkData: RouteData[]
-  ): IIconLinkData[] {
-    return externalLinkData.map((data) => {
-      const icon = (
-        <img src={data.iconSrc} width={iconWidth} alt={data.title} />
-      );
-
+      let icon = undefined;
+      if (isstring(data.iconSrc)) {
+        icon = <img src={data.iconSrc} width={iconWidth} alt={data.title} />;
+      }
+      // Assume an SVG Icon
+      else {
+        icon = <data.iconSrc sx={{ fontSize: `${iconWidth}px` }} />;
+      }
       return { title: data.title, url: data.route, icon: icon };
     });
   }
@@ -100,9 +84,6 @@ export default function Nav({
     new RouteData("/traits", "Traits", TraitsPng),
     new RouteData("/spells", "Spells", SpellsPng),
     new RouteData("/status-effects", "Status Effects", StatusEffectsPng),
-  ];
-
-  const externalLinkData = [
     new RouteData(
       "https://github.com/rovermicrover/siralim-ultimate-ui",
       "Source Code",
@@ -114,33 +95,24 @@ export default function Nav({
       "SU on Steam",
       SteamJpg
     ),
+    new RouteData(
+      "https://docs.google.com/spreadsheets/d/1RYRvKTCLLJxXrZ_7OOjG8j98L_fjE5KNHtLG4wHn9Xw/edit#gid=0",
+      "Source 1",
+      SourceIcon
+    ),
+    new RouteData(
+      "https://docs.google.com/spreadsheets/d/1qvWwf1fNB5jN8bJ8dFGAVzC7scgDCoBO-hglwjTT4iY/edit#gid=0",
+      "Source 2",
+      SourceIcon
+    ),
+    new RouteData(
+      "https://docs.google.com/spreadsheets/d/1hlS4iNB6Uj-KVKzmFEygZkeTfX-U73B9R2lXdV3d5I8/edit#gid=0",
+      "Source 3",
+      SourceIcon
+    ),
   ];
 
-  const sourceData = [
-    {
-      title: "Source 1",
-      url: "https://docs.google.com/spreadsheets/d/1RYRvKTCLLJxXrZ_7OOjG8j98L_fjE5KNHtLG4wHn9Xw/edit#gid=0",
-    },
-    {
-      title: "Source 2",
-      url: "https://docs.google.com/spreadsheets/d/1qvWwf1fNB5jN8bJ8dFGAVzC7scgDCoBO-hglwjTT4iY/edit#gid=0",
-    },
-    {
-      title: "Source 3",
-      url: "https://docs.google.com/spreadsheets/d/1hlS4iNB6Uj-KVKzmFEygZkeTfX-U73B9R2lXdV3d5I8/edit#gid=0",
-    },
-  ];
-
-  const iconLinks: IIconLinkData[] = [];
-  const sourceDataTransformed: IIconLinkData[] =
-    sourceLinksToIconLinks(sourceData);
-  const routeDataTransformed: IIconLinkData[] = routeDataToIconLinks(routeData);
-  const externalLinkDataTransformed: IIconLinkData[] =
-    externalLinkstoIconLinks(externalLinkData);
-
-  iconLinks.push(...routeDataTransformed);
-  iconLinks.push(...externalLinkDataTransformed);
-  iconLinks.push(...sourceDataTransformed);
+  const iconLinks: IIconLinkData[] = routeDataToIconLinks(routeData);
 
   return (
     <Drawer
