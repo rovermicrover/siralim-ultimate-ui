a83aebf15e730e1ede09a090164c046182c0ce43
Andrew Rove <andrew@rove.com>
2021-10-23 22:52:30 -0500

    Ensure alt text

--- src/components/SortedTableHeader.tsx
+++ src/components/SortedTableHeader.tsx
@@ -72,7 +72,7 @@ export default function SortedTableHeader({
       >
         {icon ? (
           typeof icon == "string" ? (
-            <img src={icon} />
+            <img src={icon} alt={name} />
           ) : (
             React.createElement(icon)
           )
